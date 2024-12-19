0155b1c88cee799c0f606dfd1e92c044d7df894b
cuanjarlos <castillopujantejc000@gmail.com>
2024-12-19 14:34:47 -0500

    creado main  y operadores

---
+++ Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora{
+    public static void main(String[] args) {
+        //valores predefinidos (se pueden cambiar)
+        double num1=10.5;
+        char operador='+';
+        double num2=3.2;
+    }
+}
