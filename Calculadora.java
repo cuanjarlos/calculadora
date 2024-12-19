dfe88ee600cb910816b1d3b962d5a24e0348932a
cuanjarlos <castillopujantejc000@gmail.com>
2024-12-19 14:47:32 -0500

    añadida multiplicación

--- Calculadora.java
+++ Calculadora.java
@@ -2,7 +2,18 @@ public class Calculadora{
     public static void main(String[] args) {
         //valores predefinidos (se pueden cambiar)
         double num1=10.5;
-        char operador='+';
+        char operador='*';
         double num2=3.2;
+        double resultado=0;
+        switch (operador) {
+            case '*':
+                resultado=num1*num2;
+                break;
+        
+            default:
+            System.out.println("El resultado es inválido");
+                return;
+        }
+        System.out.println("El resultado es: "+resultado);
     }
 }
